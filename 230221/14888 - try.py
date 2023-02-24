7977df7955acfb0a6c4380f0e4ca27c293d43391
dontk1llme <20190525@sungshin.ac.kr>
2023-02-24 17:46:06 +0900

    230224

--- 230221/14888 - try.py
+++ 230221/14888 - try.py
@@ -85,3 +85,40 @@ def dfs(idx):
 dfs(1)
 print(maxR)
 print(minR)
+
+#////////////////////////
+# 왜 제대로 안나옴? ????????????
+
+# N = int(input())
+# num = list(map(int, input().split())) #숫자
+# op = list(map(int, input().split())) #연산자
+# min_ = int(1e9)
+# max_ = -int(1e9)
+# ans = num[0]
+#
+# def dfs(idx):
+#     global ans
+#     global min_, max_
+#
+#     if idx==N:
+#         if ans > max_: max_ = ans
+#         if ans < min_: min_ = ans
+#         return
+#
+#     for i in range(4): #연산
+#         tmp = ans
+#         if op[i]>0:
+#             if i==0: ans+= num[idx]
+#             elif i==1: ans-=num[idx]
+#             elif i==2: ans*=num[idx]
+#             else:
+#                 if ans>=0: ans//=num[idx]
+#                 else: ans = (-ans//num[idx])*-1
+#         op[i]-=1
+#         dfs(idx+1)
+#         ans = tmp
+#         op[i]+=1 #원래 상태로 돌리기
+#
+# dfs(1)
+# print(max_)
+# print(min_)
