a30d94433ce2c1e197fdfc255fa43c4b5063762a
dontk1llme <20190525@sungshin.ac.kr>
2023-02-27 17:56:06 +0900

    230227

---
+++ 230228/[BAEKJOON] 2206-ing.py
@@ -0,0 +1,26 @@
+# 2206 벽 부수고 이동하기
+# 하나 부숴도됨. 최단거리 출력. 불가능이면 -1
+
+from collections import deque
+d = [(0,1),(0,-1),(1,0),(-1,0)]
+
+def bfs(r,c,N,M):
+    q = deque()
+    q.append((r,c))
+    crash = 0
+    while q:
+        x,y = q.popleft()
+        if x==N and y==M:
+            print(visited[x][y]-1)
+            return True
+        for i in range(4):
+            nx,ny = x+d[i][0] + y+d[i][1]
+            if
+
+
+N,M = map(int,input().split())
+lst = [list(map(int, input())) for _ in range(N)]
+r,c=1,1
+visited = [[0]*M for _ in range(N)]
+visited[r][c]=1
+bfs(r,c,N,M)
