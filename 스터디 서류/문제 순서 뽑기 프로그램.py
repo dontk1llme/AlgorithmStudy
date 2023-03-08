d0550c6a005112af61702cd9bd66d9c5413e0b0a
dontk1llme <20190525@sungshin.ac.kr>
2023-03-08 17:41:03 +0900

    230308

--- 스터디 서류/문제 순서 뽑기 프로그램.py
+++ 스터디 서류/문제 순서 뽑기 프로그램.py
@@ -1,10 +1,13 @@
 import random
 
-#9명
 member = ['이예슬', '진익근', '이성우', '송준우', '최홍준', '최태호', '정현아','서동훈','김하늘']
 random.shuffle(member)
+
+#오늘 몇 명이죠?
+N = 5 #명+1
+
 print('~*~이번 주 담당 문제 랜덤 돌리기~*~')
-for i in range(1,10):
+for i in range(1,N):
    print(f'{i}번: {member[i-1]}')
 
 print('~~~~~~~다들 파이팅~~~~~~~')
