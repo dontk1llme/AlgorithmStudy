d9bf59a33406553885dfd68aaa476810d96f6fdc
dontk1llme <20190525@sungshin.ac.kr>
2023-04-04 17:40:24 +0900

    230404

---
+++ 230404/[BAEKJOON] 2661-ing.py
@@ -0,0 +1,6 @@
+# boj 2661 좋은수열 ????????
+# 백트래킹
+# https://velog.io/@deannn/BOJ-%EB%B0%B1%EC%A4%80-2661%EB%B2%88-%EC%A2%8B%EC%9D%80-%EC%88%98%EC%97%B4-Python
+# https://namhandong.tistory.com/148
+N = int(input())
+numlst = list(range(1,4))
